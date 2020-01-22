a3d06b638b14f97c4a929289788d86f98941da3b
mhdsemps <mhdsemps@gmail.com>
2020-01-23 00:55:18 +0300

    qtn-9

---
+++ Basic-Algorithm-Scripting-Boo-who.js
@@ -0,0 +1,11 @@
+// Check if a value is classified as a boolean primitive. Return true or false.
+
+// Boolean primitives are true and false.
+
+// Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+function booWho(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  return [true,false].includes(bool);
+}
+
+booWho(null);
