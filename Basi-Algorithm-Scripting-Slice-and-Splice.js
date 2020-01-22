247c7ce724608f61edf6e3297ee8dcfd2738c2b5
mhdsemps <mhdsemps@gmail.com>
2020-01-23 02:39:34 +0300

    qtn-11

---
+++ Basi-Algorithm-Scripting-Slice-and-Splice.js
@@ -0,0 +1,20 @@
+// You are given two arrays and an index.
+
+// Use the array methods slice and splice to copy each element of the first array into the second array, in order.
+
+// Begin inserting elements at index n of the second array.
+
+// Return the resulting array. The input arrays should remain the same after the function runs.
+
+// Remember to use Read-Search-Ask if you get stuck. Write your own code.
+function frankenSplice(arr1, arr2, n) {
+  // It's alive. It's alive!
+  // console.log(arr1.join());
+  // arr2.splice(1,0,'45','67')
+  // console.log(arr2)
+  arr2.splice(n, 0, ...arr1)
+  console.log(arr2);
+  return arr2;
+ 
+}
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
